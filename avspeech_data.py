03e0b187c6dd65694f781dc32c516946408bcace
Ubuntu <ubuntu@ip-172-31-13-94.us-west-2.compute.internal>
2023-05-04 06:08:47 +0000

    error handling, dont load file into memory

--- avspeech_data.py
+++ avspeech_data.py
@@ -6,21 +6,45 @@ import face_recognition
 import datetime
 import os
 import os.path
+import shutil
+import time
 
 from pprint import pprint, pformat
 
 
-def get_face_encoding_streams(video_frames, similarity_tol=0.6):
+def get_face_encoding_streams(video_frames, x0, y0):
     """
     given video frames array, return face encoding stream (we assume only one face per video)
     """
-    detections = face_recognition.batch_face_locations(list(video_frames), batch_size=16, number_of_times_to_upsample=1)
+    ymax = video_frames.shape[1]
+    xmax = video_frames.shape[2]
+
+    t1 = time.time()
+    detections = face_recognition.batch_face_locations(list(video_frames), batch_size=20, number_of_times_to_upsample=1)
+    t2 = time.time()
+    print("Time to get detections: {}".format(t2 - t1))
 
     encoding_stream = []
     for i, frame in enumerate(video_frames):
+        # get the detection closest to x0, y0
+        best_detection = None
+        min_dist = float("inf")
+        for d in detections[i]:
+            x = d[1] - d[3]
+            y = d[2] - d[0]
+            x = x / xmax
+            y = y / ymax
+            dist = (x - x0)**2 + (y - y0)**2
+            if dist < min_dist:
+                best_detection = d
+        frame_detection = []
+        if best_detection is not None:
+            frame_detection.append(best_detection)
+
+        # retrieve encoding at given detection location
         encodings = face_recognition.face_encodings(
             frame, 
-            known_face_locations=detections[i],
+            known_face_locations=frame_detection,
             model="large"
         )
 
@@ -40,68 +64,66 @@ def generate_dataset(data_path, parent_save_path):
     FPS = 25
     dummy_path = os.path.join(parent_save_path, "dummy.mp4")
 
-    # get list of urls and start/stop times
     with open(data_path, "r") as f:
-        lines = f.readlines()
-    lines = lines[:4]
+        # dont load all lines into memory
+        line = f.readline()
+        while line:
+            # retrieve url, start time, end time, x0, y0
+            line = line.strip()
+            chunks = line.split(",")
+            url = chunks[0]
+            start = float(chunks[1])
+            end = float(chunks[2])
+            x0 = float(chunks[3])
+            y0 = float(chunks[4])
 
-    vid_infos = []
-    for line in lines:
-        line = line.strip()
-        chunks = line.split(",")
-        vid_infos.append((chunks[0], float(chunks[1]), float(chunks[2])))
-    
-    for vid_info in vid_infos:
-        url = "https://www.youtube.com/watch?v={}".format(vid_info[0])
-        start = str(datetime.timedelta(seconds=vid_info[1]))
-        end = str(datetime.timedelta(seconds=vid_info[2]))
+            # retrieve true video url
+            with youtube_dl.YoutubeDL({'format': 'best'}) as ydl:
+                try:
+                    result = ydl.extract_info(url, download=False)
+                except:
+                    print("Failed to load video information")
+                    line = f.readline()
+                    continue
 
-        # find true video url
-        with youtube_dl.YoutubeDL({'format': 'best'}) as ydl:
-            try:
-                result = ydl.extract_info(url, download=False)
-            except:
-                print("Failed to load video information")
-                continue
+            true_urls = [f['url'] for f in result['formats'] if f['acodec'] != 'none' and f['vcodec'] != 'none']
+            formats = [f for f in result['formats'] if f['acodec'] != 'none' and f['vcodec'] != 'none']
+            true_url = true_urls[-1]
 
-        true_urls = [f['url'] for f in result['formats'] if f['acodec'] != 'none' and f['vcodec'] != 'none']
-        formats = [f for f in result['formats'] if f['acodec'] != 'none' and f['vcodec'] != 'none']
-        true_url = true_urls[-1]
+            save_path = os.path.join(parent_save_path, "{}".format(url))
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
 
-        save_path = os.path.join(parent_save_path, "{}".format(vid_info[0]))
-        if not os.path.exists(save_path):
-            os.mkdir(save_path)
+                # handle failure to process video
+                try:
+                    # download video clip into dummy file using ffmpeg
+                    subprocess.run('ffmpeg -nostdin -ss {} -to {} -i "{}" -filter:v fps={} {}'.format(start, end, true_url, FPS, dummy_path), shell=True)
+                    
+                    # get video frames, process with face_recognition, save streams of face encodings as individual csv files
+                    video_frames = skvideo.io.vread(dummy_path)
+                    stream = get_face_encoding_streams(video_frames, x0, y0)
+                    stream_path = os.path.join(save_path, "encoding_stream.csv")
+                    np.savetxt(stream_path, stream, delimiter=",")
 
-            # download video clip into dummy file using ffmpeg
-            subprocess.run('ffmpeg -nostdin -ss {} -to {} -i "{}" -filter:v fps={} {}'.format(start, end, true_url, FPS, dummy_path), shell=True)
+                    # save audio as seperate .wav file
+                    audio_path = os.path.join(save_path, "audio.wav")
+                    subprocess.run("ffmpeg -nostdin -i {} -vn {}".format(dummy_path, audio_path), shell=True)
+                except:
+                    # delete directory
+                    print("failed to process video {}".format(url))
+                    shutil.rmtree(save_path)
 
-            # save video clip information
-            info = skvideo.io.ffprobe(dummy_path)
-            info_str = pformat(info, indent=4)
-            info_path = os.path.join(save_path, "info.txt")
-            with open(info_path, 'w') as f:
-                f.write(info_str)
-            
-            # get video frames, process with face_recognition, save streams of face encodings as individual csv files
-            video_frames = skvideo.io.vread(dummy_path)
-            stream = get_face_encoding_streams(video_frames)
-            stream_path = os.path.join(save_path, "encoding_stream.csv")
-            np.savetxt(stream_path, stream, delimiter=",")
+                # delete dummy file
+                if os.path.exists(dummy_path):
+                    os.remove(dummy_path)
 
-            # save audio as seperate .wav file
-            audio_path = os.path.join(save_path, "audio.wav")
-            subprocess.run("ffmpeg -nostdin -i {} -vn {}".format(dummy_path, audio_path), shell=True)
-
-            # delete dummy file
-            os.remove(dummy_path)
+            line = f.readline()
 
 
 
 if __name__ == "__main__":
 
     generate_dataset(
-        data_path="../AVSpeech/avspeech_test.csv",
-        parent_save_path="../avspeech_data"
+        data_path="./data/avspeech_train.csv",
+        parent_save_path="./data/dataset/"
     )
-
-    pass
