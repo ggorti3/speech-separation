97a35eae5e92f72f42ff2a9a146d10af6cb70c04
Ubuntu <ubuntu@ip-172-31-13-94.us-west-2.compute.internal>
2023-05-05 08:39:44 +0000

    synthetic_data.py

---
+++ synthetic_data.py
@@ -0,0 +1,14 @@
+import skvideo.io
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class TwoSpeakerData(Dataset):
+    def __init__(self, dataset_path):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, idx):
+        pass
